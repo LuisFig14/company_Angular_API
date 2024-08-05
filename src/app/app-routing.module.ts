252a6d9e2b3128d6eb7ee01add56abfdf63329cb
Fernando Licea <fernandolic1407@gmail.com>
2024-08-04 19:59:07 -0600

    components general

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { ServiciosComponent } from './components/servicios/servicios.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { LoginComponent } from './components/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component:HomeComponent},
+  {path:'Servicios', component:ServiciosComponent},
+  {path:'Contact', component:ContactComponent},
+  {path:'Login', component:LoginComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,18 +6,24 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HeaderComponent } from './components/header/header.component';
+import { ServiciosComponent } from './components/servicios/servicios.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     FooterComponent,
-    HeaderComponent
-  ],
+    HeaderComponent,
+    ServiciosComponent,
+    ContactComponent,
+    LoginComponent,
+    ],
   imports: [
     BrowserModule,
-    AppRoutingModule
-  ],
+    AppRoutingModule,
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
